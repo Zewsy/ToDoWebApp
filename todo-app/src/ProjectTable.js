b5cc07499ea664fd2d013ab00acfa63468645f8d
Zewsy <47224879+Zewsy@users.noreply.github.com>
2019-10-14 00:02:43 +0200

    Project fetch added
    HomePage fetching projects from db.json

--- todo-app/src/ProjectTable.js
+++ todo-app/src/ProjectTable.js
@@ -1,14 +1,57 @@
 import React from 'react';
+import './projects.css';
+
+class Project extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            id: this.props.id,
+            name: this.props.name,
+            description: this.props.description
+        }
+    }
+    render(){
+        return(
+            <tr>
+                {this.state.name} <br />
+                {this.state.description}
+            </tr>
+        );
+    }
+}
 
 class ProjectTable extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            projects: []
+        };
+    }
+
+    componentDidMount(){
+        const url = "http://localhost:3001/projects";
+        fetch(url)
+            .then(resp => resp.json())
+            .then(data =>
+                {this.setState({projects: data});
+            })
+    }
+
     render(){
+        const projects = this.state.projects.map(
+        p => {
+        return (<tr className="projectBar">
+            <Project id={p.id}
+                    name={p.name}
+                    description={p.description}
+            />
+        </tr>)});
+
         return(
             <div>
                 <h1>Projects</h1>
                 <table className="projectsTable">
-                    <tr>
-                        SampleProject
-                    </tr>
+                    {projects}
                 </table>
             </div>
         );
